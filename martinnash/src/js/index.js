7ef37b0aa11041aaf071874ee0e4ea78fb0a1462
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-17 11:59:29 -0400

    final commit for mock up 1

--- martinnash/src/js/index.js
+++ martinnash/src/js/index.js
@@ -1,31 +1,37 @@
-// Javascript Entry Point
+import $ from "jquery";
+
+ 
+
+(function(){
+ 
+var counter = 0,
+$items = document.querySelectorAll('.diy-slideshow figure'),
+numItems = $items.length;
+ 
+var showCurrent = function(){
+var itemToShow = Math.abs(counter%numItems);
+ 
+[].forEach.call( $items, function(el){
+el.classList.remove('show');
+});
+ 
+$items[itemToShow].classList.add('show');
+};
+ 
+document.querySelector('.next').addEventListener('click', function() {
+counter++;
+showCurrent();
+}, false);
+ 
+document.querySelector('.prev').addEventListener('click', function() {
+counter--;
+showCurrent();
+}, false);
+ 
+})();
+ 
 
 
 
-// <script>
-// var myImage= new Array(); 
-// myImage[0]="1.jpg";       
-// myImage[1]="11.jpg";
-// myImage[2]="14.jpg";
-// myImage[3]="17.jpg"; 
-
-// var ImageCnt = 0;
-
-// function next(){
-//     ImageCnt++;
-//     document.getElementById("whiteBox").style.background = 'url(' + myImage[ImageCnt] + ')';
-//   }
-// </script>
 
 
-function changeImage(current) {
-	var imagesNumber = 5;
-
-	for (i=1; i<=imagesNumber; i++) {
-		if (i == current) {
-			document.getElementById("normal" + current).style.display = "block";
-		} else {
-			document.getElementById("normal" + i).style.display = "none";
-		}
-	}
-}
